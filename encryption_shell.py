0ad01dcfd8ebba15b0c5c574d225823eff70f96c
KamenB <k.brestnichki@gmail.com>
2017-05-28 22:53:32 +0100

    Usage tweak

--- encryption_shell.py
+++ encryption_shell.py
@@ -5,12 +5,6 @@ import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
 
-# Bind input to raw_input for Python 2 support
-try:
-   input = raw_input
-except NameError:
-   pass
-
 class AESCipher(object):
 
     def __init__(self, key): 
@@ -36,24 +30,31 @@ class AESCipher(object):
     def _unpad(s):
         return s[:-ord(s[len(s)-1:])]
 
-f = open("shared_secret.txt", "r")
-secret = f.readlines()[0].strip()
-aesCipher = AESCipher(secret)
+if __name__ == '__main__':
+    # Bind input to raw_input for Python 2 support
+    try:
+       input = raw_input
+    except NameError:
+       pass
 
-while True:
-    print("Choose action: Encryption[e]/Decryption[d]")
-    command = input()
-    if command == "e":
-        print("Insert message to encrypt:")
-        message = input()
-        ciphertext = aesCipher.encrypt(message)
-        ciphertext = str(ciphertext)
-        # Remove stuff in version 3
-        if ciphertext[:2] == "b'":
-            ciphertext = ciphertext[2:-1]
-        print("\n" + ciphertext + "\n")
-    elif command == "d":
-        print("Insert cyphertext to decrypt:")
-        ciphertext = input()
-        message = aesCipher.decrypt(ciphertext)
-        print("\n" + message + "\n")
+    f = open("shared_secret.txt", "r")
+    secret = f.readlines()[0].strip()
+    aesCipher = AESCipher(secret)
+
+    while True:
+        print("Choose action: Encryption[e]/Decryption[d]")
+        command = input()
+        if command == "e":
+            print("Insert message to encrypt:")
+            message = input()
+            ciphertext = aesCipher.encrypt(message)
+            ciphertext = str(ciphertext)
+            # Remove stuff in version 3
+            if ciphertext[:2] == "b'":
+                ciphertext = ciphertext[2:-1]
+            print("\n" + ciphertext + "\n")
+        elif command == "d":
+            print("Insert cyphertext to decrypt:")
+            ciphertext = input()
+            message = aesCipher.decrypt(ciphertext)
+            print("\n" + message + "\n")
